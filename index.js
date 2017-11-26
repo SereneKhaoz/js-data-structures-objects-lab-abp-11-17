532bcaff0474ac7732a320faa8d1f6d9ab2b6f9c
Dynisha Faust <dgeekgirl08@gmail.com>
2017-11-26 23:43:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 // Write your solution in this file!
-const driver = {name}
-
-function updateDriverWithKeyAndValue(object, key, value){
+const driver = {
   name: 'Sam',
   address: '11 Broadway'
+
+}
+
+function updateDriverWithKeyAndValue(object, key, value){
+  
 
 newObj;
 }
